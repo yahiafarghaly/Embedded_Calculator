1d896b9861dc6e010912a17721ba610195acc02c
Ahmed Mokhtar <mokhtar92@gmail.com>
2015-09-29 20:45:19 +0200

    substantial algorithm rewrite that works

--- Calculator/Calculator/Calculator.c
+++ Calculator/Calculator/Calculator.c
@@ -12,7 +12,7 @@
 #include "calculator_blocks.h"
 
 
-	uint8 Flag=StartPoint;/*Contains the state of the program flow*/
+	uint8 Flag;/*Contains the state of the program flow*/
 
 int main(void)
 {
@@ -22,21 +22,6 @@ int main(void)
     while(1)
     {
 		ResetData();
-		LCD_clearScreen();
-		Flag=StartPoint;
-		 while(Flag==StartPoint) GetData(); /*Don't get out until pressing equal sign*/
-		 while(Flag==AnalyzePoint) Analyze(); /*Don't get out until separating every thing correctly*/
-		 while(Flag==CalculatePoint) Calculate(); /*Don't get out until calculate the value*/
-		 while(Flag==DisplayPoint) DisplayResult(); /*Display the result*/
-		 	/*If an error is found in one of these stages, the Flag will be sit to "Error point" and will set 
-			 to "StartPoint" again in the start of the loop after clearing every thing*/
+		GetData(); /*Don't get out until pressing equal sign*/
 	}
-	
-	
 }
-
-
-
-
-
-
--- Calculator/Calculator/calculator_blocks.h
+++ Calculator/Calculator/calculator_blocks.h
@@ -18,25 +18,14 @@
 #include "math.h"
 
 /*The limits on the inputs which you are set*/
-#define MaxNumberOfDigits 16
-#define MaxNumberOfOperands 12
-#define MaxNumberOfOperations MaxNumberOfOperands-1
+#define MaxNumberOfDigits 15
+#define CalculatorDynamic 0
 
-#define IsOperation(n) ((n=='*')||(n=='+')||(n=='-')||(n=='%'))
-
-enum StageFlag{Reset,StartPoint,AnalyzePoint,CalculatePoint,DisplayPoint,ErrorPoint};
-enum keys{EnterKey=13,EqualKey= '=' ,Numberkey,OperationKey};
-enum Errors{ExcessedMaxNumberOfdigits,InvalidInput,OutofRange};	
+enum StageFlag{Reset, Intercept};
+enum keys{EnterKey=13,EqualKey= '='};
 
 extern uint8 Flag;
 
-extern uint32 powerOf(uint8 x,uint8 y); /*Used With ConvertToInt64()*/
 void ResetData(); /*To get all the globel variables in .c file set to zero when doing new operation*/
 void GetData(); /*Get the data from the keypad until pressing "equal key"*/
-void SynchronousDisplay(uint8 data); /*To display what you type in the LCD while storing the inputs*/
-void Analyze(); /*To get the operands seperated from the operation to be able to know the operations which i am going to use*/
-sint64 ConvertToInt64(uint8 * ptr,uint8 len); /*Converting series of seperate digits into a single number (1,1,5)=>115 */
-void ThrowException(uint8 *msg);/*Throw error message on LCD when error found*/
-void Calculate(); /*Do the math*/
-void DisplayResult();
 #endif /* CALCULATOR_BLOCKS_H_ */
